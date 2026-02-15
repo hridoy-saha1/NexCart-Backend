348ff2453f521147e72d433b5d72483742069dfc
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-02-15 16:20:41 +0600

    Add admin module routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CustomerModule } from './customer/customer.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [AdminModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
