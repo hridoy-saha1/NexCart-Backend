f0398245213690a0d20c2e18e30a37673be16a68
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2026-05-11 14:13:49 +0600

    add

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -146,7 +146,7 @@ export class AdminController {
 
   // DELETE
   @Delete(':id')
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.adminService.remove(id);
   }
