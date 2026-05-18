4e86935361eff3923fee63bbd585d735dcafa2d9
𝙳𝚒𝚙𝚝𝚊 𝙿𝚛𝚊𝚝𝚝𝚘𝚢 𝙺𝚊𝚛𝚖𝚊𝚔𝚊𝚛 <145040028+Dipta395@users.noreply.github.com>
2026-05-18 20:24:21 +0600

    merge

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -11,10 +11,12 @@ import { PassportModule } from '@nestjs/passport';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
 import { PusherModule } from '../pusher/pusher.module';
+import { Delivery } from 'src/rider/delivery.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AdminEntity, Rider, Order]),
+    TypeOrmModule.forFeature([AdminEntity, Rider, Order, Delivery]),
+    PusherModule,
     //  LOAD ENV FILE
     ConfigModule.forRoot({
       isGlobal: true,
@@ -35,17 +37,12 @@ import { PusherModule } from '../pusher/pusher.module';
         },
       },
     }),
-import { Rider } from 'src/rider/rider.entity';
-import { Order } from 'src/customer/order.entity';
-import { PusherModule } from 'src/pusher/pusher.module';
-import { Delivery } from 'src/rider/delivery.entity';
-
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([AdminEntity, Rider, Order, Delivery]),
-    PusherModule,
   ],
   controllers: [AdminController],
   providers: [AdminService, JwtStrategy],
 })
 export class AdminModule {}
+
+// import { Rider } from 'src/rider/rider.entity';
+// import { Order } from 'src/customer/order.entity';
+// import { PusherModule } from 'src/pusher/pusher.module';
